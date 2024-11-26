ffba24a7516a39a0a7bba198e251eba19ef1c2ce
Adam Wojturski <adik96885@gmail.com>
2024-11-26 16:31:36 +0100

    Add main tests

--- Binary_Heap/Binary_Heap.cpp
+++ Binary_Heap/Binary_Heap.cpp
@@ -1,7 +1,74 @@
 #include <iostream>
+#include <random>
+#include <chrono>
 #include "BH.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct some_object {
+	int field_1;
+	char field_2;
+};
+
+std::string some_objects_str(some_object* so) {
+	return "(" + std::to_string(so->field_1) + ", " + so->field_2 + ")";
+}
+
+bool some_objects_cmp1(some_object* so1, some_object* so2) {
+	return so1->field_1 > so2->field_1;
+}
+
+bool some_objects_cmp2(some_object* so1, some_object* so2) {
+	return so1->field_1 == so2->field_1;
+}
+
+int main() {
+	const int MAX_ORDER = 6;
+	const int LETTES_SIZE = 26;
+	const char LETTERS[LETTES_SIZE] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+	static std::random_device rd;
+	static std::default_random_engine dre(rd());
+	std::uniform_int_distribution<int> rnd_num(0, MAX_ORDER * 1000);
+	std::uniform_int_distribution<int> rnd_let(0, LETTES_SIZE - 1);
+
+	BH::BinHeap<some_object*>* bh = new BH::BinHeap<some_object*>();
+
+	for (int i = 1; i <= MAX_ORDER; i++) {
+		std::cout << "--------------------------------" << std::endl;
+		std::cout << "Test: " << i << std::endl << std::endl;
+
+		int n = pow(10, i);
+
+		std::chrono::high_resolution_clock::time_point start_time = std::chrono::high_resolution_clock::now();
+		for (int j = 0; j < n; j++) {
+			some_object* so = new some_object();
+			so->field_1 = rnd_num(dre);
+			so->field_2 = LETTERS[rnd_let(dre)];
+			bh->Push(so, some_objects_cmp1);
+			so = nullptr;
+		}
+		std::chrono::high_resolution_clock::time_point end_time = std::chrono::high_resolution_clock::now();
+
+		std::chrono::duration<double> pushing_time = end_time - start_time;
+		std::cout << "Pushing time: " << pushing_time.count() << "s" << std::endl;
+		std::cout << bh->ToString(3, some_objects_str);
+
+		start_time = std::chrono::high_resolution_clock::now();
+		for (int j = 0; j < n; j++) {
+			some_object* so = bh->ExtractRoot();
+			delete so;
+		}
+		end_time = std::chrono::high_resolution_clock::now();
+
+		std::chrono::duration<double> extracting_time = end_time - start_time;
+		std::cout << "Extracting time: " << extracting_time.count() << "s" << std::endl;
+		std::cout << bh->ToString(3, some_objects_str);
+
+		double total_time = pushing_time.count() + extracting_time.count();
+		std::cout << "Total time: " << total_time << "s" << std::endl;
+
+		bh->Erase();
+	}
+
+	delete bh;
+	return 0;
 }
