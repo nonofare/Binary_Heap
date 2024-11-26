207b58252ea186f7b7f7a37167e1e6375c45af9a
Adam Wojturski <adik96885@gmail.com>
2024-11-26 16:31:01 +0100

    Define the class structure

--- Binary_Heap/BH.h
+++ Binary_Heap/BH.h
@@ -1,1 +1,82 @@
 #pragma once
+#include <string>	
+#include "DA.h"
+
+namespace BH {
+
+	template <typename T>
+	class BinHeap {
+
+		DA::DynArr<T> heap;
+
+		size_t NodeParentIndex(size_t index) const {
+			return (index - 1) / 2;
+		}
+
+		size_t NodeLeftIndex(size_t index) const {
+			return (2 * index + 1);
+		}
+
+		size_t NodeRightIndex(size_t index) const {
+			return (2 * index + 2);
+		}
+
+		void HeapifyUp() {
+
+		}
+
+		void HeapifyDown() {
+
+		}
+
+	public:
+		BinHeap() {
+
+		}
+
+		~BinHeap() {
+			heap.Erase();
+		}
+
+		size_t Size() const {
+			return heap.Size();
+		}
+
+		size_t Capacity() const {
+			return heap.Capacity();
+		}
+
+		bool IsEmpty() const {
+			return heap.IsEmpty();
+		}
+
+		bool Push(T data, bool (*cmp)(T, T) = nullptr) {
+
+		}
+
+		bool Pop(size_t index = size - 1) {
+
+		}
+
+		T ExtractRoot(bool (*cmp)(T, T) = nullptr) {
+
+		}
+
+		bool Erase() {
+			return heap.Erase();
+		}
+
+		const T& operator[](size_t index) const {
+			return heap[index];
+		}
+
+		std::string ToString(unsigned int limit = 0, std::string(*str)(T) = nullptr) const {
+			std::string text = ">>> Binary Heap <<<\n";
+			text += "based on: \n";
+
+			text += heap.ToString(limit, str);
+
+			return text;
+		}
+	};
+}
